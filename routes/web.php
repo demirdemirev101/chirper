debf75a50a6a3d1d0076a7089bb405ab988587ff
demir <demirdemirev101@gmail.com>
2025-10-21 14:12:57 +0300

    Add ChirpController, Chirp model, views and migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::middleware('auth')->group(function() {
+
+    Route::post('/chirps', [ChirpController::class, 'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}',[ChirpController::class, 'destroy']);
 });
+
+//Route::resource('/chirps', ChirpController::class)
+//->only(['store', 'edit', 'update', 'destroy']);
+
+//REGISTER ROUTES
+Route::view('/register','auth.register')
+->middleware('guest')
+->name('register');
+
+Route::post('/register', RegisterController::class)
+->middleware('guest');
+
+//LOGOUT
+Route::post('/logout', LogoutController::class)
+->middleware('auth')
+->name('logout');
+
+//LOGIN
+Route::view('/login', 'auth.login')
+->middleware('guest')
+->name('login');
+
+Route::post('/login', LoginController::class)
+->middleware('guest')
+->name('login');
